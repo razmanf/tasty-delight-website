aa8f1ecccde0cf678548ac9ac2680d91a0876bc5
Razman <mrazman123@gmail.com>
2026-07-21 09:44:52 +0530

    fix: perfect authentication UI spacing and symmetrical shadows

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen pt-[450px] max-[470px]:pt-[95vw] sm:pt-[522px] min-[1200px]:pt-0 pb-3 flex flex-col min-[1200px]:justify-center items-center min-[1200px]:px-8">
+<div class="min-h-screen pt-[418px] max-[470px]:pt-[87vw] sm:pt-[448px] min-[1200px]:pt-0 pb-6 min-[1200px]:pb-3 flex flex-col min-[1200px]:justify-center items-center min-[1200px]:px-8">
     <div>
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-xl mt-2 px-6 py-4 bg-white shadow-md sm:rounded-lg">
+    <div class="w-full sm:max-w-xl mt-2 mb-6 min-[1200px]:mb-0 px-6 py-4 bg-white shadow-[0_0_15px_rgba(0,0,0,0.1)] sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
